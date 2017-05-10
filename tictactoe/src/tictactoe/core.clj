ebda558906478819fe62399f98bd0dfe82d403e8
Satvik <nsatvik@gmail.com>
2017-05-10 08:26:51 +0530

    tic-tac-toe board basic funcs

---
+++ tictactoe/src/tictactoe/core.clj
@@ -0,0 +1,32 @@
+(ns tictactoe.core
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(def board-size 3)
+(def board [["X"  "O"  " "] [" "  " "  " "] [" "  " "  " "]])
+
+(defn mark-cell
+  "Mark the cell in the board"
+  [board row col val]
+  {:pre [(< 0 row board-size) (< 0 col board-size)]}
+  (assoc-in board [row col] val))
+
+(defn read-cell
+  "Returns the element at a cell in board"
+  [board row col]
+  {:pre [(< 0 row board-size) (< 0 col board-size)]}
+  (get-in board [x y]))
+
+(defn row-str
+  [row]
+  (str " " (str/join " | " row) " "))
+
+(defn print-board 
+  "Prints the board"
+  [board]
+  (print (str/join "\n" (for [row board] (row-str row)))))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
