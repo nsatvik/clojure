5a6e249b4c9dbae6a5f0810f7775bc57b4ee2ea1
Satvik <nsatvik@gmail.com>
2017-06-01 13:32:03 +0530

    ttt - checking for draw

--- tictactoe/src/tictactoe/core.clj
+++ tictactoe/src/tictactoe/core.clj
@@ -2,16 +2,15 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(def board-size 2) ;starts from 0
-(def board [["X"  "O"  " "] [" "  " "  " "] [" "  " "  " "]])
+(def board [["X"  "X"  "X"] ["O"  "O"  "O"] ["X"  "X"  "O"]])
 
 (defn mark-cell
   "Mark the cell in the board"
   [board row col val]
-  {:pre [(<= 0 row board-size) (<= 0 col board-size) (= " " (get-in board [row col]))]}
+  {:pre [(<= 0 row 2) (<= 0 col 2) (= " " (get-in board [row col]))]}
   (assoc-in board [row col] val))
 
-(defn read-cell
+(defn get-cell
   "Returns the element at a cell in board"
   [board row col]
   {:pre [(<= 0 row board-size) (<= 0 col board-size)]}
@@ -21,12 +20,34 @@
   [row]
   (str " " (str/join " | " row) " "))
 
+
 (defn print-board 
   "Prints the board"
   [board]
   (print (str/join "\n" (for [row board] (row-str row)))))
 
+(defn find-winner
+  "Find the winner if any"
+  [board]
+  (print "Finds the winner if any"))
+
+(defn draw? 
+  "Checks if it is a draw based on blank counts"
+  [board]
+  (zero? (apply + (map (fn [m] (m " " 0)) (map frequencies board)))))
+
+
+
+(defn start-game
+  "Starts the game"
+  []
+  (println "Starts the game"))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Enter :")
+  (def input (read-line))
+  (println (int input))
+  (println "End"))
